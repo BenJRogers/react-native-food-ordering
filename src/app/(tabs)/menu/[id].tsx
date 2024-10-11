02c9b6b88953406c08a8a4f2136624d93189dc86
Benjamin Rogers <benjrogers48@gmail.com>
2024-10-12 08:51:43 +1100

    configured navigation

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { View, Text } from "react-native";
 
@@ -6,6 +6,8 @@ const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
   return (
     <View>
+      <Stack.Screen options={{ title: `Details: ${id}` }} />
+
       <Text style={{ fontSize: 20 }}>Product Details for Screen ID: {id}</Text>
     </View>
   );
