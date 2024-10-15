206e912da2fd81a588ec12a1e1713ff8ae18107e
Benjamin <benjamin.rogers@merkleinc.com.au>
2024-10-15 22:48:39 +1100

    adding CartProvider

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, Platform } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useContext } from "react";
+import { CartContext } from "@/providers/CartProvider";
 
 const CartScreen = () => {
+  const {} = useContext(CartContext);
   return (
     <View>
-    <Text>CartScreen</Text>
+      <Text>CartScreen</Text>
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 };
