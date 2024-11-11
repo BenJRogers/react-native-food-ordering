27b1df1bbec0d385285a305631eb8d153bbd6da6
Benjamin Rogers <benjrogers48@gmail.com>
2024-11-12 08:40:26 +1100

    updated expo/start refactor order list items

--- src/components/OrderListItem.tsx
+++ src/components/OrderListItem.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Link, useSegments } from "expo-router";
 import { Order } from "@/types";
+import relativeTime from "dayjs/plugin/relativeTime";
 import dayjs from "dayjs";
 
 type OrderItemProps = {
   order: Order;
 };
 
-const timeOrderd = (orderedTime: string) => {
-  return dayjs().diff(orderedTime, "hour");
-};
+dayjs.extend(relativeTime);
 
 const OrderList = ({ order }: OrderItemProps) => {
+  const segments = useSegments();
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.idCopy}>Order #{order.id}</Text>
-        <Text style={styles.timeCopy}>
-          {timeOrderd(order.created_at)} hours ago
-        </Text>
-      </View>
-      <Text style={styles.statusCopy}>{order.status}</Text>
-    </View>
+    <Link href={`/${segments[0]}/orders/${order.id}`} asChild>
+      <Pressable style={styles.container}>
+        <View>
+          <Text style={styles.idCopy}>Order #{order.id}</Text>
+          <Text style={styles.timeCopy}>
+            {dayjs(order.created_at).fromNow()}
+          </Text>
+        </View>
+        <Text style={styles.statusCopy}>{order.status}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -37,17 +41,17 @@ const styles = StyleSheet.create({
   },
   idCopy: {
     fontWeight: "bold",
-    fontSize: 16,
+    fontSize: 14,
     marginBottom: 6,
   },
   timeCopy: {
     color: "grey",
-    fontSize: 16,
+    fontSize: 14,
   },
 
   statusCopy: {
     fontWeight: "bold",
-    fontSize: 16,
+    fontSize: 14,
   },
 });
 
