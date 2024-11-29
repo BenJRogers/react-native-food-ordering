96433331c633cadb4fb4a385d522a0f511b2ee19
Benjamin Rogers <benjrogers48@gmail.com>
2024-11-29 23:36:40 +1100

    setup intial supabase setup

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://nfmbepepaanvzcrthdpp.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mbWJlcGVwYWFudnpjcnRoZHBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4ODMxNDcsImV4cCI6MjA0ODQ1OTE0N30.cFu0ef6h-vRbW4DgA0B6LrKO749xpUteOqubLLIXV4g";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
