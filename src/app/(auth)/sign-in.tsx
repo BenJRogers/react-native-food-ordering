c2b0e0cff035a0ada67d7cc9dee6b09cc4aa1059
Benjamin Rogers <benjrogers48@gmail.com>
2024-12-09 14:13:05 +1100

    auth provider

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -2,13 +2,18 @@
 import React from "react";
 import { useRouter } from "expo-router";
 import AuthForm from "@components/AuthForm";
+import { supabase } from "@/lib/supabase";
+import { Alert } from "react-native";
+
 
 const SignInScreen = () => {
   const router = useRouter();
-
-  const handleSignIn = (email: string, password: string) => {
-    console.log("Email: ", email);
-    console.log("Password: ", password);
+  const handleSignIn = async (email: string, password: string) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) Alert.alert(error.message);
   };
 
   return (
